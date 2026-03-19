8483d97fafb01991c9bbdb294c3ad172b78e679f
Logan Kim <logeonwoo05@gmail.com>
2026-03-19 17:28:14 +0900

    과제1 코딩과 테스트 완

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,27 @@
 namespace EchoMessenger
 {
-    public partial class Form1 : Form
+    public partial class EchoMessenger : Form
     {
-        public Form1()
+        public EchoMessenger()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // TextBox에 입력된 텍스트를 가져와서 변수에 저장
+            string typed_msg = myTxtBox.Text;
+
+            // 가져온 문자열을 ListBox의 Items으로 추가
+            lstMsgWindow.Items.Add(typed_msg);
+
+            // 다음 입력을 위해 TextBox의 내용을 비우기
+            myTxtBox.Clear();
         }
     }
 }
