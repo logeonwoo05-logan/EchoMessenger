3636d47e2791efbadb1fd51e2464fed94bec0f11
Logan Kim <logeonwoo05@gmail.com>
2026-03-19 17:28:14 +0900

    과제1 코딩과 테스트 완료
    ListBox를 중앙에 TextBox를 ListBox 아래에 배치함
    전송 버튼을 누르면 TextBox에 입력한 내용이 ListBox로 들어가 표시되고 TextBox의 내용은 깨끗하게 지워짐

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EchoMessenger
 {
-    partial class Form1
+    partial class EchoMessenger
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblText = new Label();
+            lstMsgWindow = new ListBox();
+            btnSend = new Button();
+            myTxtBox = new TextBox();
+            SuspendLayout();
+            // 
+            // lblText
+            // 
+            lblText.AutoSize = true;
+            lblText.Location = new Point(34, 22);
+            lblText.Name = "lblText";
+            lblText.Size = new Size(190, 32);
+            lblText.TabIndex = 0;
+            lblText.Text = "Echo Messenger";
+            lblText.Click += label1_Click;
+            // 
+            // lstMsgWindow
+            // 
+            lstMsgWindow.FormattingEnabled = true;
+            lstMsgWindow.Location = new Point(66, 78);
+            lstMsgWindow.Name = "lstMsgWindow";
+            lstMsgWindow.Size = new Size(644, 292);
+            lstMsgWindow.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            btnSend.Location = new Point(538, 376);
+            btnSend.Name = "btnSend";
+            btnSend.Size = new Size(150, 46);
+            btnSend.TabIndex = 2;
+            btnSend.Text = "전송";
+            btnSend.UseVisualStyleBackColor = true;
+            btnSend.Click += btnSend_Click;
+            // 
+            // myTxtBox
+            // 
+            myTxtBox.Location = new Point(66, 383);
+            myTxtBox.Name = "myTxtBox";
+            myTxtBox.Size = new Size(428, 39);
+            myTxtBox.TabIndex = 3;
+            // 
+            // EchoMessenger
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(808, 486);
+            Controls.Add(myTxtBox);
+            Controls.Add(btnSend);
+            Controls.Add(lstMsgWindow);
+            Controls.Add(lblText);
+            Name = "EchoMessenger";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblText;
+        private ListBox lstMsgWindow;
+        private Button btnSend;
+        private TextBox myTxtBox;
     }
 }
